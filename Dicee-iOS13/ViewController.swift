2631f6885cc057196a5a946d9065baab73aae056
Dheyson Alves <dheyson.silva@zappts.com.br>
2022-05-21 18:08:52 -0300

    feat: adding visual and business logic for the dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -5,16 +5,23 @@
 //  Created by Angela Yu on 11/06/2019.
 //  Copyright © 2019 London App Brewery. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let arrayOfDices = ["DiceOne", "DiceTwo", "DiceThree","DiceFour","DiceFive","DiceSix"];
+                     
+        diceImageView1.image = UIImage(named: arrayOfDices[Int.random(in: 0...arrayOfDices.count - 1)])
+        diceImageView2.image = UIImage(named: arrayOfDices[Int.random(in: 0...arrayOfDices.count - 1)])
+        
     }
-
-
+    
 }
 
